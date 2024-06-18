0df9f62b04681593d506d700739d3e5d01d5526b
AdityaNG <AdityaNG@users.noreply.github.com>
2024-06-18 16:29:40 +0000

    ✅ Ready to clone and code.

---
+++ socc_plotter/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for socc_plotter."""
+
+from socc_plotter.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from socc_plotter.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "socc_plotter"
